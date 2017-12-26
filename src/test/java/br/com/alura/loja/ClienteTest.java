27b1ffb9954c505ea648783b412d22b0423b0812
marcioasan <marcioasan@gmail.com>
2017-12-26 21:51:48 -0200

    Initial commit

---
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -0,0 +1,71 @@
+package br.com.alura.loja;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Projeto;
+
+public class ClienteTest {
+
+	private HttpServer server;
+	
+	@Before
+	public void startaServidor() {
+		this.server = Servidor.inicializaServidor();
+	}
+	
+    @After
+    public void mataServidor() {
+        server.stop();
+    }
+//	@Test
+//	public void testaQueAConexaoComOServidorFunciona() {
+//
+//		Client client = ClientBuilder.newClient();
+//		WebTarget target = client.target("http://www.mocky.io");
+//		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
+//		System.out.println(conteudo);
+//		Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));
+//		
+//	}
+//	
+//	@Test
+//    public void testaQueAConexaoComOServidorFuncionaNoPathDeProjetos() {
+//        Client client = ClientBuilder.newClient();
+//        WebTarget target = client.target("http://localhost:8080");
+//        String conteudo = target.path("/projetos").request().get(String.class);
+//        Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
+//    }
+	
+	@Test
+	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
+
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/carrinhos").request().get(String.class);
+		System.out.println(conteudo);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+		
+		 server.stop();
+	}
+	
+    @Test
+    public void testaQueBuscarUmProjetoTrazOProjetoEsperado() {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080");
+        String conteudo = target.path("/projetos").request().get(String.class);
+        Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+        Assert.assertEquals("Minha loja", projeto.getNome());
+    }
+}
